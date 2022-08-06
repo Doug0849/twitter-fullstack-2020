5f343fdc37c3149f2a96c344582d0fdcd0a0559e
choyz1024 <nagatoyuki1024@gmail.com>
2022-08-06 17:48:23 +0800

    feat: reactivate like btn

--- public/javascripts/infinite-scroll-tweets.js
+++ public/javascripts/infinite-scroll-tweets.js
@@ -27,6 +27,7 @@ async function lazyLoading () {
     })
     const newObserveTarget = document.getElementById('tweets-end')
     observer.observe(newObserveTarget)
+    activeLikeBtn()
     page++
   } catch (error) {
     console.error(error)
@@ -197,4 +198,30 @@ function getEndHTML () {
     </div>
   </div>
   `
+}
+
+function activeLikeBtn () {
+  const likeSubmitBtn = document.querySelectorAll('.like-submit-btn')
+  const likedIcon = document.querySelectorAll('.like') || ''
+  const likedNumbers = document.querySelectorAll('.like-count') || ''
+  likeSubmitBtn.forEach((btn, i) => {
+    btn.addEventListener('click', async e => {
+      try {
+        const tweetId = btn.dataset.tweetid || ''
+        if (btn.matches('.liked')) {
+          likedNumbers[i].textContent--
+          btn.classList.toggle('liked')
+          likedIcon[i].classList.toggle('outline-like')
+          await axios.post(`/tweets/${tweetId}/unlike`)
+        } else {
+          likedNumbers[i].textContent++
+          btn.classList.toggle('liked')
+          likedIcon[i].classList.toggle('outline-like')
+          await axios.post(`/tweets/${tweetId}/like`)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    })
+  })
 }
