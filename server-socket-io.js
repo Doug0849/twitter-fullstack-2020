3454adf35c9b5a4c2ce28a40f01a9e7ac3c5a4ae
doug0849 <etaomonk@gmail.com>
2022-08-07 20:56:54 +0800

    heroku test

--- server-socket-io.js
+++ server-socket-io.js
@@ -59,21 +59,21 @@ io.on('connect', async socket => {
     const messageLastIdIndex = messagesId.length - 1
     const messageId = messagesId[messageLastIdIndex]
     // 確認是否最後一個訊息沒有讀，如果找不出資料代表沒讀
-    const noReadMsg = await Readuser.findAll({
-      where: { messageId: messageId, readId: userId },
-      raw: true
-    })
-    // 就從上一個有找到的message_id + 1開始，到最後一個，全部寫入，如果有找出代表有讀過
-    if (!noReadMsg[0]) {
-      messagesId.map(async id => {
-        if (id > lastReadMessage[0].messageId) {
-          return await Readuser.create({
-            messageId: id,
-            readId: userId
-          })
-        }
-      })
-    }
+    // const noReadMsg = await Readuser.findAll({
+    //   where: { messageId: messageId, readId: userId },
+    //   raw: true
+    // })
+    // // 就從上一個有找到的message_id + 1開始，到最後一個，全部寫入，如果有找出代表有讀過
+    // if (!noReadMsg[0]) {
+    //   messagesId.map(async id => {
+    //     if (id > lastReadMessage[0].messageId) {
+    //       return await Readuser.create({
+    //         messageId: id,
+    //         readId: userId
+    //       })
+    //     }
+    //   })
+    // }
 
     // 將使用者資料存入socket.data
     const userList = []
