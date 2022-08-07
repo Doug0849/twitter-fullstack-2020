2fca5f9592c1da1780a913c111f0bce01174f5c6
doug0849 <etaomonk@gmail.com>
2022-08-07 20:30:04 +0800

    heroku test

--- server-socket-io.js
+++ server-socket-io.js
@@ -56,12 +56,14 @@ io.on('connect', async socket => {
         })
       })
     }
+    const messageLastIdIndex = messagesId.length - 1
     // 確認是否最後一個訊息沒有讀，如果找不出資料代表沒讀
     // 就從上一個有找到的message_id + 1開始，到最後一個，全部寫入，如果有找出代表有讀過
-    // const lastNoReadMessage = await Readuser.findAll({
-    //   where: { messageId: messagesId[messagesId.length - 1], readId: userId },
-    //   raw: true
-    // })
+    const lastNoReadMessage = await Readuser.findAll({
+      where: { messageId: messagesId[messageLastIdIndex], readId: userId },
+      raw: true
+    })
+    console.log(lastNoReadMessage)
     // if (!lastNoReadMessage[0]) {
     //   messagesId.map(async id => {
     //     if (id > lastReadMessage[0].messageId) {
